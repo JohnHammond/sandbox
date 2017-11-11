c75ad3ebad190323982a6c1b76a6bbf332f7158d
SANDBOX CAPSTONE <sandbox1@SANDBOXs-MacBook-Pro.local>
2017-11-11 01:22:09 -0800

    Added Glade test for Linux

---
+++ glade/run.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import gi
+from gi.repository import Gtk
+import re
+
+builder = Gtk.Builder()
+builder.add_from_file( 'test.glade' )
+
+
+def ip_entry_activate_cb( widget):
+
+	global builder
+	
+	content = widget.get_text().strip()
+	
+	if ( not re.match( r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', content ) ):
+		error = builder.get_object('error')
+		error.set_text('Please use an IPv4 address.')
+	else:
+		print "Success!"
+	
+
+
+handlers = {
+	"ip_entry_activate_cb": ip_entry_activate_cb,
+}
+
+
+
+
+builder.connect_signals(handlers)
+
+window = builder.get_object('window')
+window.show_all()
+window.connect('destroy', Gtk.main_quit)
+Gtk.main()
+
